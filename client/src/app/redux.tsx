5c8cb3d4bcc87f0b4983e84a65edae32c0193e58
JackScottKing <jackscottking3@gmail.com>
2024-11-06 16:29:40 +0200

    Fixed linting problem with Redux Toolkit

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useRef } from "react";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import {
@@ -26,18 +28,17 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 /* REDUX PERSISTENCE */
 const createNoopStorage = () => {
   return {
-    getItem(_key: string) {
+    getItem(_key: any) {
       return Promise.resolve(null);
     },
-    setItem(_key: string, value: unknown) {
+    setItem(_key: any, value: any) {
       return Promise.resolve(value);
     },
-    removeItem(_key: string) {
+    removeItem(_key: any) {
       return Promise.resolve();
     },
   };
 };
-
 
 const storage =
   typeof window === "undefined"
