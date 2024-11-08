2c4922bd675c6307eb684fc633a6047f0a02620c
JackScottKing <jackscottking3@gmail.com>
2024-11-08 17:37:45 +0200

    401 Unathorised error fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,36 +14,17 @@ import teamRoutes from "./routes/teamRoutes";
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
-
-// CORS Configuration
-const corsOptions = {
-  origin: 'https://master.d3rpe2d2b8wltm.amplifyapp.com', // Adjust this to your frontend domain for better security
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: ['Authorization', 'Content-Type', 'Accept', 'Origin'],
-};
-app.use(cors(corsOptions));
-
-// Handle OPTIONS preflight requests globally
-app.options('*', (req, res) => {
-  res.set({
-    'Access-Control-Allow-Origin': 'https://master.d3rpe2d2b8wltm.amplifyapp.com',
-    'Access-Control-Allow-Headers': 'Authorization,Content-Type,Accept,Origin',
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
-  });
-  res.status(204).send();
-});
-
-// Other middlewares
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 /* ROUTES */
 app.get("/", (req, res) => {
-  res.send("This is the home route");
+  res.send("This is home route");
 });
 
 app.use("/projects", projectRoutes);
@@ -55,5 +36,5 @@ app.use("/teams", teamRoutes);
 /* SERVER */
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on part ${port}`);
 });
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,20 +1,11 @@
 import { Router } from "express";
+
 import { getUser, getUsers, postUser } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", getUsers);
 router.post("/", postUser);
-router.get("/", getUser);
-
-// Adding CORS pre-flight handler
-router.options("", (req, res) => {
-res.set({
-'Access-Control-Allow-Origin': 'https://master.d3rpe2d2b8wltm.amplifyapp.com',
-'Access-Control-Allow-Headers': 'Authorization,Content-Type,Accept,Origin',
-'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
-});
-res.send();
-});
+router.get("/:cognitoId", getUser);
 
 export default router;
